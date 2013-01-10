5a7522d0464be7145046b65413b1a85e9bcbe521
Alana <weberam@plu.edu>
2013-01-10 10:47:46 -0800

    Added checkWin() and the beginnings of processMove() as well as Player

--- Game Engine/GameEngine.java
+++ Game Engine/GameEngine.java
@@ -54,5 +54,103 @@ public class GameEngine {
 		result[1] = destFleets;
 		return result;
 	}
+        
+        /**
+         * Check to see if anyone has won the game yet.
+         * @return  ID of the player who has won, or -1 if no one has
+         */
+        public int checkWin() {
+            int winner = -1;
+            
+            //check to see if one Player owns all the Planets
+            int candidate = gs.getPlanets()[0].getOwner(); //grab the ID of the owner of the first planet
+            for(Planet p : gs.getPlanets()) {
+                if (p.getOwner() == candidate) winner = candidate; //check to see if all other planets
+                else winner = -1;                                   //are owned by this player
+            }
+            
+            return winner;
+            //Is there any other way someone can win??
+        }
 	
+        /**
+         * Process a move coming from a Player. Note that this is in its preliminary stages.
+         * This method is really long.
+         * @param move  The move from the User
+         * @return 
+         */
+        public GameChange processMove(Move move) {
+            GameChange gc = new GameChange(0,0,0); //TODO Replace these 0's with the turn variables
+            
+            //make sure the Player ID is valid
+            boolean validPlayer = false;
+            for(int p : gs.getPlayerList()) {
+                if (move.getPlayerID() == p && move.getPlayerID() != 0)
+                    validPlayer = true;
+            }
+            if(!validPlayer)
+                throw new RuntimeException("Invalid player.");
+            
+            //TODO make sure that it is indeed that player's turn
+                    
+           //loop through the mini Moves
+            while(move.hasNext()) {
+                int[] miniMove = move.next();
+                
+                //validate the mini-Move
+                if(miniMove.length != 3)
+                    throw new RuntimeException("Invalid move.");
+                
+                //name variables for ease of use
+                //TODO check for invalid Planet IDs
+                Planet source = gs.getPlanetByID(miniMove[0]);
+                Planet dest = gs.getPlanetByID(miniMove[1]);
+                int numFleets = miniMove[2];
+                int player = move.getPlayerID();
+                
+                //Source = Dest: Deployment
+                if(source.getIDNum() == dest.getIDNum()) {
+                    //for right now, check that the Planet is neutral
+                    if(source.getOwner() != 0)
+                        throw new RuntimeException("Invalid move. Attempt to deploy to non-neutral Planet.");
+                    source.addFleets(numFleets);
+                    source.setOwner(player);
+                    gc.addChange(source);
+                }                
+                //Source owner = Dest owner: Reinforcement
+                else if(source.getOwner() == dest.getOwner()) {
+                    source.addFleets((-1)*numFleets);
+                    dest.addFleets(numFleets);
+                    gc.addChange(source);
+                    gc.addChange(dest);
+                }
+                //Source != planet: Attack
+                //Assume a defender victory is never possible because of the retreat policy
+                else {
+                    int[] results = processAttack(numFleets, dest.getFleets());
+                    if(results[1] == 0) {//the attacker has won
+                        dest.setOwner(source.getOwner());
+                        dest.setFleets(results[0]);
+                        source.addFleets((-1)*numFleets);
+                        gc.addChange(source);
+                        gc.addChange(dest);
+                    }
+                    else { //the attacker retreated
+                        source.setFleets(results[0]);
+                        dest.setFleets(results[1]);
+                        gc.addChange(source);
+                        gc.addChange(dest);
+                    }
+                }
+                
+            }
+            
+            int winner = checkWin();
+            //TODO what to do if someone has won? Call some sort of endGame() method
+            
+            //update the Gamestate
+            gs.update(gc);
+            
+            return gc;
+        }
 }
